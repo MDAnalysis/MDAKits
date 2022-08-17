2fe53dff5bc8beac316bdf8f720ecac32ca7e99f
IAlibay <ialibay@mdanalysis.org>
2022-08-17 03:48:59 +0100

    write statuses to json

---
+++ utils/write-ci-status-json.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+# MIT License
+
+# Copyright (c) 2022 MDAnalysis Development Team
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""
+Script to write out errors to a JSON file for temporary storage.
+"""
+import argparse
+import os
+import json
+from typing import List, Dict
+
+
+parser = argparse.ArgumentParser(
+    description="Get metadata yaml info",
+)
+
+parser.add_argument(
+    "--tag",
+    type=str,
+    help="Name to tag errors with",
+)
+
+parser.add_argument(
+    "--mdakit",
+    type=str,
+    help="name of mdakit we are writing errors for",
+)
+
+
+def get_statuses(tag: str, env_values: List[str]) -> Dict[str, str]:
+
+    status_dict = {}
+
+    for entry in env_values:
+        status_dict[f"{entry}-{tag}"] = os.environ[entry]
+
+    return status_dict
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    env_statuses = ['python_bounds', 'mdakit_install', 'tests_install',
+                    'mda_install', 'test_out']
+
+    status_dict = get_statuses(args.tag, env_statuses)
+
+    outfile = f"{args.mdakit}-{args.tag}-statuses.json"
+
+    with open(outfile, 'w') as f:
+        json.dump(status_dict, f)
